e51bc481bcb80f5e3b248109d37ceb217c79a473
troy520 <troy520@gmail.com>
2019-05-24 14:41:19 +0800

    試試看上傳Git

--- src/ch01/ATasteOfKotlin.kt
+++ src/ch01/ATasteOfKotlin.kt
@@ -16,11 +16,17 @@ fun main(args: Array<String>) {
     val y = findYoung()
     println("The oldest is $oldest")
     println("The youngest person is $youngest") //這會取得物件，並且自動toString
-    println("The youngest function is $y" ) // 這會取得方法物件， toString 出代稱
+    println("The youngest function is $y") // 這會取得方法物件， toString 出代稱
 }
 
-fun findOld() = {persons.maxBy { it.age ?: 0 }}
+fun findOld() = { persons.maxBy { it.age ?: 0 } }
 
-fun findYoung() = {persons.minBy { it.age ?: 0 }}
+fun findYoung() = { persons.minBy { it.age?:-1 } }
 
+/**
+ * 取出這個人的年紀
+ */
+fun getAge(person: Person): Int? {
+   return person.age
+}
 // The oldest is: Person(name=Bob, age=29)
---
+++ src/main/tw/com/haitec/test/Test.java
@@ -0,0 +1,17 @@
+package main.tw.com.haitec.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("用 Java 真的要打好多字啊！");
+    }
+
+    /**
+     * A test for cross kotlin-java method call.
+     */
+    public static String getIntro() {
+
+        return "I'm a man in black.";
+
+    }
+
+}
